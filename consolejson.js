45d243a7978135d1d0265cbacfe3bc99583abc85
Leschonander <33600597+Leschonander@users.noreply.github.com>
2018-02-17 16:22:31 -0500

    WebScraper
    Combines Console.save(); code with JQUERY to download a pages <p> elements to be converted into json files.

---
+++ consolejson.js
@@ -0,0 +1,32 @@
+// Automaticcly downloads a page's <p> elements as a JSON via JQUERY 
+(function(console){
+
+    console.save = function(data, filename){
+
+        if(!data) {
+            console.error('Console.save: No data')
+            return;
+        }
+
+        if(!filename) filename = 'console.json'
+
+        if(typeof data === "object"){
+            data = JSON.stringify(data, undefined, 4)
+        }
+
+        var blob = new Blob([data], {type: 'text/json'}),
+            e    = document.createEvent('MouseEvents'),
+            a    = document.createElement('a')
+
+        a.download = filename
+        a.href = window.URL.createObjectURL(blob)
+        a.dataset.downloadurl =  ['text/json', a.download, a.href].join(':')
+        e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
+        a.dispatchEvent(e)
+    }
+})(console)
+var script = document.createElement('script');script.src = "https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js";document.getElementsByTagName('head')[0].appendChild(script);
+
+
+var str = $("p").text();
+console.save(str);
