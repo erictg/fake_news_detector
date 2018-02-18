010e40d9a8f10bbfe35c866d97c43e5e6710dde9
erictg <ericsolender@gmail.com>
2018-02-17 20:03:06 -0500

    cors fix

--- src/fake_news/stream_api/main.go
+++ src/fake_news/stream_api/main.go
@@ -5,13 +5,14 @@ import (
 	"fake_news/stream_api/service"
 	"log"
 	"net/http"
+	"github.com/rs/cors"
 )
 
 func main(){
 	router := mux.NewRouter()
 
 	router.HandleFunc("/rest/stream", service.Post).Methods("POST")
-
-	log.Fatal(http.ListenAndServe(":8001", router))
+	c := cors.AllowAll()
+	log.Fatal(http.ListenAndServe(":8001", c.Handler(router)))
 
 }
